17a2ae0c6dde1d34cf0074bb0bbfc3bcfd583394
unknown <fmyondaime2@gmail.com>
2022-08-28 21:40:34 -0300

    Segundo ejercicio de Estructura de datos

--- ejercicio.py
+++ ejercicio.py
@@ -7,10 +7,20 @@ print(f'El nombre de usuario es: {nombre} y su DNI es: {dni}')
 
 
 
-# Primer ejercicio de Estructura de datos
+# Primer ejercicio de Estructura de datos (SUMA)
 
 num1 = 10
 num2 = 20
 suma = num1 + num2
 
 print(suma)
+
+
+
+# Segundo ejercicio de Estructura de datos (Cadena de textos)
+
+nombre = 'Beimar'
+apellido = 'Delgado'
+nomApell = nombre + ' ' + apellido
+
+print(nomApell)
