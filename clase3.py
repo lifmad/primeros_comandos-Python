5ca3671c1dd4b28766871f4458dd212009ec42e0
unknown <fmyondaime2@gmail.com>
2022-09-14 22:49:37 -0300

    segundo ejercicio de funciones

--- clase3.py
+++ clase3.py
@@ -4,4 +4,15 @@ def area_rectangulo(b,h):
     area = b * h
     return area
 
-print(area_rectangulo(10,10))
+print(area_rectangulo(10,7))
+
+
+# Ejercicio 2
+
+pi = 3.14159
+
+def area_circulo(r):
+    circulo = pi * r **2
+    return circulo
+
+print(area_circulo(5))
