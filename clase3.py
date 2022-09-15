899ac5eeaaed75c58d965132fd9dea7d466e98a9
unknown <fmyondaime2@gmail.com>
2022-09-14 22:53:41 -0300

    tercer ejercicio de funciones

--- clase3.py
+++ clase3.py
@@ -16,3 +16,17 @@ def area_circulo(r):
     return circulo
 
 print(area_circulo(5))
+
+
+# Ejercicio 3 
+
+
+def relacion(num1,num2):
+    if (num1 > num2):
+        return 1
+    elif (num1 < num2):
+        return -1
+    else:
+        return 0
+
+print(relacion(1,2))                
